64ac01940f2d3f77037e6f1533f2de7c0b4e3083
NancyMurangira <shimwemurangiranancy@gmail.com>
2024-08-15 08:20:34 +0300

    Added changes

--- src/app/components/Login/index.tsx
+++ src/app/components/Login/index.tsx
@@ -1,24 +1,53 @@
 "use client"
-import React, {ChangeEvent, useState} from "react";
-
-const Login =() =>{
-    const [username, SetUsername] = useState('');
-    const [password, SetPassword] = useState('');
-
+import { ChangeEvent, FormEvent, useState } from "react";
+import { userLogin } from "@/app/utils/userLogin";
+const Login = () =>{
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState('');
+    const [response, setResponse] = useState('');
+    const handleLogin = async(event: React.FormEvent<HTMLFormElement>)=>{
+        event.preventDefault()
+        try{
+            setLoading(true);
+            const response = await userLogin({username, password})
+            setResponse(response.message ?? 'Successful Login')
+            console.log({response});
+            setLoading(false);
+        }
+        catch(error){
+            setLoading(false);
+            const message = (error as Error).message;
+            console.log({message});
+        }
+    };
     return(
-        <form>
-            <h2> LOGIN </h2>
-            <input placeholder="Enter username" type="text" required className="border border-gray-500 rounded-md w-full py-4 px-2 mt-2 " onChange={(event:ChangeEvent<HTMLInputElement>)=> SetUsername(event.target.value)}/>
-            <br/>
-            <input placeholder="Enter password" type="password" className="border border-gray-500 rounded-md w-full py-4 px-2 my-4"onChange={(event:ChangeEvent<HTMLInputElement>)=> SetPassword(event.target.value)} required/>
-            <br/>
-            <button type="submit" className="bg-green-500 text-white py-2 px-3 rounded-md" >Submit</button>
-        </form>
+        <div>
+            <form className="w-2/5 border p-10" onSubmit={handleLogin}>
+            <h2>LOGIN</h2>
+            <input type="text"
+            placeholder="Enter Username"
+            required
+            className="border w-full px-4 py-6 border-gray-500 rounded-xl mt-4 text-black"
+            onChange={(event: ChangeEvent<HTMLInputElement>)=>setUsername(event.target.value)}
+            />
+            <br />
+            <input type="password"
+            placeholder="Enter Password"
+            required
+            className="border w-full px-4 py-6 border-gray-500 rounded-xl my-4 text-black"
+            onChange={(event: ChangeEvent<HTMLInputElement>)=>setPassword(event.target.value)}
+            />
+            <br />
+            <button type="submit"
+            className="rounded-xl bg-green-500 text-white cursor-pointer px-6 py-4">
+            {loading? 'Loading....': 'Submit'}
+                </button>
+                {response && <p className="text-green-500 text-sm">{response}</p>}
+                {error && <p className="text-red-500 text-sm">{error}</p>}
+            </form>
+        </div>
     )
-}
- 
- export default Login;
-
-
-
- 
+};
+export default Login;
